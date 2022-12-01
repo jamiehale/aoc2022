ca4a5df5ffe9406971aaeb03ddffddc113b2741e
Jamie Hale <jamiesonhale@gmail.com>
2022-12-01 09:54:04 -0500

    Update part1

--- 1/rust/part1/src/main.rs
+++ 1/rust/part1/src/main.rs
@@ -1,13 +1,21 @@
 use std::cmp;
 use std::{io, io::prelude::*};
 
+fn line_to_option(line: String) -> Option<i32> {
+    if line.is_empty() {
+        None
+    } else {
+        Some(line.parse::<i32>().unwrap())
+    }
+}
+
 fn main() {
     let mut max_calories = 0;
     let mut group_total = 0;
     for line in io::stdin().lock().lines() {
-        match line.unwrap().parse::<i32>() {
-            Ok(n) => group_total += n,
-            Err(_) => {
+        match line_to_option(line.unwrap()) {
+            Some(n) => group_total += n,
+            None => {
                 max_calories = cmp::max(max_calories, group_total);
                 group_total = 0;
             }

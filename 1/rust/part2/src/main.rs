d976a59804a98e31459bd34df378026fa489f9b6
Jamie Hale <jamiesonhale@gmail.com>
2022-12-01 09:51:54 -0500

    Slightly more rusty

--- 1/rust/part2/src/main.rs
+++ 1/rust/part2/src/main.rs
@@ -1,21 +1,31 @@
 use std::{io, io::prelude::*};
 
+fn line_to_option(line: String) -> Option<i32> {
+    if line.is_empty() {
+        None
+    } else {
+        Some(line.parse::<i32>().unwrap())
+    }
+}
+
+fn add_and_update(n: i32, totals: &mut Vec<i32>) {
+    totals.push(n);
+    totals.sort_unstable_by(|a, b| b.cmp(a));
+    totals.truncate(3);
+}
+
 fn main() {
     let mut max_calories = vec![0, 0, 0];
     let mut group_total = 0;
     for line in io::stdin().lock().lines() {
-        match line.unwrap().parse::<i32>() {
-            Ok(n) => group_total += n,
-            Err(_) => {
-                max_calories.push(group_total);
-                max_calories.sort_unstable_by(|a, b| b.cmp(a));
-                max_calories.truncate(3);
-                group_total = 0;
+        match line_to_option(line.unwrap()) {
+            Some(n) => group_total += n,
+            None => {
+                add_and_update(group_total, &mut max_calories);
+                group_total = 0
             }
         }
     }
-    max_calories.push(group_total);
-    max_calories.sort_unstable_by(|a, b| b.cmp(a));
-    max_calories.truncate(3);
+    add_and_update(group_total, &mut max_calories);
     println!("{}", max_calories.iter().sum::<i32>());
 }

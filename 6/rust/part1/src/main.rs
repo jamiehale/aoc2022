579164e9c5623f3e3689687412cbc217ad770ad6
Jamie Hale <jamiesonhale@gmail.com>
2022-12-06 19:01:00 -0500

    Add day6

---
+++ 6/rust/part1/src/main.rs
@@ -0,0 +1,30 @@
+use itertools::Itertools;
+use std::collections::VecDeque;
+use std::{io, io::prelude::*};
+
+fn is_deque_unique(deque: &VecDeque<char>, length: usize) -> bool {
+    deque.into_iter().unique().collect::<Vec<&char>>().len() == length
+}
+
+fn main() {
+    let mut deque: VecDeque<char> = VecDeque::new();
+    for (i, c) in io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>()[0]
+        .chars()
+        .enumerate()
+    {
+        deque.push_back(c);
+
+        while deque.len() > 4 {
+            deque.pop_front();
+        }
+
+        if is_deque_unique(&deque, 4) {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+}
